8b2a0bd6920ff57b43eabbb0c33a69cf80056137
vamsiteruvai <teruvaivamsi9@gmail.com>
2025-08-25 00:51:30 +0530

    upload

---
+++ index.js
@@ -0,0 +1,79 @@
+const todoInput = document.getElementById("TodoText");
+const addBtn = document.getElementById("add");
+const ul = document.querySelector("ul");
+const counter = document.getElementById("counter");
+const cn=document.getElementById('counter');
+
+
+addBtn.addEventListener("click", () => {
+    if (todoInput.value.trim() === "") {
+        alert("Please enter a task!");
+        return;
+    }
+
+    // create new todo item
+    const li = document.createElement("li");
+    li.className =
+        "text-sm flex justify-between bg-gray-800 p-2 rounded-md text-gray-200 mt-2";
+    li.innerHTML = `
+    <span>${todoInput.value}</span>
+    <div>
+      <i class="fa-solid fa-pen-to-square mx-1 cursor-pointer"></i>
+      <i class="fa-solid fa-trash mx-1 cursor-pointer" style="color: #ee3211;"></i>
+    </div>
+  `;
+    ul.appendChild(li);
+
+    const editing = document.createElement("div");
+    const input = document.createElement("input");
+    const saveBtn = document.createElement("button");
+
+    editing.className = "mt-1 flex hidden";
+    input.className = "bg-gray-800 p-1 w-[100%] rounded-lg text-sm outline-none";
+    input.placeholder = "Edit your todo";
+    saveBtn.className =
+        "text-sm p-1 bg-green-700 rounded-md text-gray-200 cursor-pointer ml-1";
+    saveBtn.textContent = "Save";
+
+    editing.appendChild(input);
+    editing.appendChild(saveBtn);
+    ul.appendChild(editing);
+
+    todoInput.value = "";
+
+    // get icons from this li
+    const editIcon = li.querySelector(".fa-pen-to-square");
+    const deleteIcon = li.querySelector(".fa-trash");
+    const span = li.querySelector("span");
+
+    // edit icon click
+    editIcon.addEventListener("click", () => {
+        input.value = span.textContent;
+        editing.classList.toggle("hidden");
+    });
+
+    // save button click
+    saveBtn.addEventListener("click", () => {
+        if (input.value.trim() === "") {
+            alert("Please enter text");
+            return;
+        }
+        span.textContent = input.value; // update todo text
+        editing.classList.add("hidden");
+    });
+
+    //counting 
+    cn.textContent=(ul.children.length)/2;
+    var ele=ul.children.length/2;
+
+    // delete icon click
+    deleteIcon.addEventListener("click", () => {
+        li.remove();
+        ele--;
+        if(ele<0)
+            cn.textContent=0;
+        else
+            cn.textContent=ele;
+        editing.remove();
+    });
+});
